c5aa76ff6a000ba5dfa106e69188ce80cca1a815
lubin <lu.bin.lu.bin@gmail.com>
2019-09-21 23:48:28 -0400

    FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import "./App.css";
 
-const App: React.FC = () => {
+interface User {
+  id: string;
+  name: string;
+}
+
+const App: FC<User> = ({ id, name, children }, context) => {
+  // return null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {id} {name}
+      {children}
     </div>
   );
-}
+};
 
 export default App;
